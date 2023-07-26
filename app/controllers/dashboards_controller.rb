11301d3c7b58f66f6334ab91162fef2a2d71aa82
('misheckcodes') <('mishecklivingi@live.com')>
2023-07-26 12:07:48 +0000

    Jul 26, 2023, 12:07 PM

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,63 @@
+class DashboardsController < ApplicationController
+  before_action :set_dashboard, only: %i[ show edit update destroy ]
+
+  def index
+    @projects = Project.all
+    @tasks = Task.all
+    @teams = Team.all
+  end
+
+  def show
+  end
+
+  def new
+    @dashboard = Dashboard.new
+  end
+
+  def edit
+  end
+
+  def create
+    @dashboard = Dashboard.new(dashboard_params)
+
+    respond_to do |format|
+      if @dashboard.save
+        format.html { redirect_to dashboard_url(@dashboard), notice: "Dashboard was successfully created." }
+        format.json { render :show, status: :created, location: @dashboard }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @dashboard.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @dashboard.update(dashboard_params)
+        format.html { redirect_to dashboard_url(@dashboard), notice: "Dashboard was successfully updated." }
+        format.json { render :show, status: :ok, location: @dashboard }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @dashboard.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @dashboard.destroy
+
+    respond_to do |format|
+      format.html { redirect_to dashboards_url, notice: "Dashboard was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_dashboard
+      @dashboard = Dashboard.find(params[:id])
+    end
+
+    def dashboard_params
+      params.fetch(:dashboard, {})
+    end
+end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,6 @@
 class Team < ApplicationRecord
   # Team associations
   has_many :users, dependent: :destroy
-  has_many :tasks
 
   # Team validations
   validates :name, presence: true
