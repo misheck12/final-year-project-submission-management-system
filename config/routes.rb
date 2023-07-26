7720a110c47c3b27a6ea538222d43aeb56aa8e97
('misheckcodes') <('mishecklivingi@live.com')>
2023-07-26 14:15:50 +0000

    Jul 26, 2023, 2:15 PM

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config.merge(path: 'admin')
-  ActiveAdmin.routes(self)
-  devise_for :users, path: 'users'
+  devise_for :users, skip: :all
 
   authenticated :user do
     root to: 'dashboards#index', as: :authenticated_root
   end
+
   root to: 'homes#index'
 
   resources :teams
@@ -17,8 +16,19 @@ Rails.application.routes.draw do
   get 'faculty/index'
   get 'faculty/show'
 
-  # Devise routes
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  # Devise routes for users
+# Devise routes for users
+devise_scope :user do
+  get '/users/sign_in', to: 'devise/sessions#new', as: :new_user_session
+  post '/users/sign_in', to: 'devise/sessions#create', as: :user_session
+  delete '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+
+  get '/users/sign_up', to: 'devise/registrations#new', as: :new_user_registration
+  post '/users', to: 'devise/registrations#create', as: :user_registration
+end
+
+
+  # Devise routes for admin users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
