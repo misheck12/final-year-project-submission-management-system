d25727cf12653bfccfabea9211b0e5e2cbf9c2c3
('misheckcodes') <('mishecklivingi@live.com')>
2023-07-26 10:00:55 +0000

    Jul 26, 2023, 10:00 AM

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,25 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, except: [:home]
-    
-    def home
-      redirect_to dashboard_path if user_signed_in?
-    end
-    
-    def dashboard
-        # Retrieve data for the dashboard
-        @projects = current_user.projects
-        @tasks = current_user.tasks
-        @teams = current_user.teams
-        
-        # Render the dashboard view
-        render 'dashboard', locals: { projects: @projects, tasks: @tasks, teams: @teams }
-    end
-      
-    
-    protected
-    
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
-    end
+  before_action :authenticate_user!, except: [:home]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def home
+    redirect_to dashboard_path if user_signed_in?
   end
-  
+    
+  def dashboard
+    # Retrieve data for the dashboard
+    @projects = current_user.projects
+    @tasks = current_user.tasks
+    @teams = current_user.teams
+
+    # Render the dashboard view
+    render 'dashboard', locals: { projects: @projects, tasks: @tasks, teams: @teams }
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role])
+  end
+end
