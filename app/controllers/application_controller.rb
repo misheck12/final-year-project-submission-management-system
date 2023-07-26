b680f640d15d0393f597285b310ad6b5ec662dec
('misheckcodes') <('mishecklivingi@live.com')>
2023-07-26 17:21:02 +0000

    Jul 26, 2023, 5:21 PM

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:home]
+  before_action :authenticate_user!, unless: :devise_controller_or_active_admin_resource?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def home
     redirect_to dashboard_path if user_signed_in?
   end
-    
+
   def dashboard
     # Retrieve data for the dashboard
     @projects = current_user.projects
@@ -17,6 +17,14 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
+  def devise_controller_or_active_admin_resource?
+    devise_controller? || active_admin_resource?
+  end
+
+  def active_admin_resource?
+    self.class.to_s.split("::").first=="ActiveAdmin"
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
