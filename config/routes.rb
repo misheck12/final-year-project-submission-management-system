ec6d29dc06c4a0f64f8aa70a632ee67e6ada4ab5
('misheckcodes') <('mishecklivingi@live.com')>
2023-07-26 14:11:38 +0000

    Jul 26, 2023, 2:11 PM

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :admin_users, ActiveAdmin::Devise.config.merge(path: 'admin')
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, path: 'users'
 
   authenticated :user do
     root to: 'dashboards#index', as: :authenticated_root
