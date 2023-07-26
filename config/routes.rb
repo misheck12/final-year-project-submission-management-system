06166fc7597d8f20be09a6991ac9e12ab7769596
('misheckcodes') <('mishecklivingi@live.com')>
2023-07-26 14:17:07 +0000

    Jul 26, 2023, 2:17 PM

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   # Devise routes for users
 # Devise routes for users
+# Devise routes for users
 devise_scope :user do
   get '/users/sign_in', to: 'devise/sessions#new', as: :new_user_session
   post '/users/sign_in', to: 'devise/sessions#create', as: :user_session
@@ -25,7 +26,13 @@ devise_scope :user do
 
   get '/users/sign_up', to: 'devise/registrations#new', as: :new_user_registration
   post '/users', to: 'devise/registrations#create', as: :user_registration
+  
+  get '/users/password/new', to: 'devise/passwords#new', as: :new_user_password
+  post '/users/password', to: 'devise/passwords#create', as: :user_password
+  get '/users/password/edit', to: 'devise/passwords#edit', as: :edit_user_password
+  put '/users/password', to: 'devise/passwords#update'
 end
+
 
 
   # Devise routes for admin users
