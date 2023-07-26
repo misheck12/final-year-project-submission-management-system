4a130aed27312cf44c8bcfeb1e3e1bf80906fd28
('misheckcodes') <('mishecklivingi@live.com')>
2023-07-26 11:47:46 +0000

    Jul 26, 2023, 11:47 AM

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,28 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # Guest user
-
-    if user.faculty?
+    user ||= User.new # guest user (not logged in)
+    
+    if user.admin?
       can :manage, :all
+    elsif user.faculty?
+      can :read, :all
+      can :create, Comment
+      can [:update, :destroy], Comment, user_id: user.id
+      can :manage, [Project, Task]
+      cannot :manage, User
+    elsif user.staff?
+      can :read, :all
+      can :manage, [Project, Task, Resource]
+      cannot :manage, User
     elsif user.student?
+      can :read, :all
+      can :manage, Comment, user_id: user.id
       can :manage, Project, user_id: user.id
-      can :manage, Task, project: { user_id: user.id }
+      can :manage, Task, user_id: user.id
+      cannot :manage, User
     else
-      # Guest user can't do anything
+      # Guest user permissions
     end
   end
 end
