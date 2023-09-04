8f0e437af0b602b539d0829415498599505d3407
Misheck Livingi <livingim2018@gmail.com>
2023-09-04 13:02:28 +0000

    Sep 4, 2023, 4:02 PM

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,20 @@ class User < ApplicationRecord
   has_many :assigned_tasks, class_name: 'Task', foreign_key: 'assignee_id'
   has_many :projects, dependent: :destroy
 
-
   # User roles for authorization with Cancancan
-  enum role: { student: 0, faculty: 1, staff: 2 }
+  enum role: { student: 0, faculty: 1, staff: 2, admin: 3 }
 
   validates :name, presence: true
   validates :role, presence: true
   validates :email, uniqueness: true
 
+  before_create :assign_admin_if_first_user
+
+  private
+
+  def assign_admin_if_first_user
+    if User.count.zero?
+      self.role = 'admin'
+    end
+  end
 end
