bc649f73e8df32a66475e726efdf4e9b0e37231e
Misheck Livingi <livingim2018@gmail.com>
2023-09-04 15:13:39 +0000

    Sep 4, 2023, 6:13 PM

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,7 @@ Rails.application.routes.draw do
   resources :teams
   resources :tasks
   resources :projects
-  resources :dashboards do
-    resources :users 
-    end
+  resources :dashboards
 
   # Define your faculty routes here
   get 'faculty/index'
@@ -21,4 +19,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  # Move user resources outside of the dashboards resources block
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
 end
