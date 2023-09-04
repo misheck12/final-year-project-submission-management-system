b4e76bda0bc25df9c9de2ad728e22e96e62e1ae1
Misheck Livingi <livingim2018@gmail.com>
2023-09-04 14:30:59 +0000

    Sep 4, 2023, 5:30 PM

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,12 @@
 class DashboardsController < ApplicationController
-  before_action :set_dashboard, only: %i[ show edit update destroy ]
+  before_action :set_dashboard, only: %i[show edit update destroy]
 
   def index
     @projects = Project.all
     @tasks = Task.all
     @teams = Team.all
+    @users = User.all # Load all users for admin actions
+    @new_user = User.new # Initialize a new user for user creation
   end
 
   def show
@@ -52,12 +54,43 @@ class DashboardsController < ApplicationController
     end
   end
 
-  private
-    def set_dashboard
-      @dashboard = Dashboard.find(params[:id])
-    end
+  # Create a new user
+  def create_user
+    @user = User.new(user_params)
 
-    def dashboard_params
-      params.fetch(:dashboard, {})
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to dashboards_url, notice: "User was successfully created." }
+        format.json { render :index, status: :created, location: dashboards_url }
+      else
+        format.html { render :index, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
+  end
+
+  # Delete a user
+  def delete_user
+    @user = User.find(params[:user_id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to dashboards_url, notice: "User was successfully deleted." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_dashboard
+    @dashboard = Dashboard.find(params[:id])
+  end
+
+  def dashboard_params
+    params.fetch(:dashboard, {})
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :role)
+  end
 end
