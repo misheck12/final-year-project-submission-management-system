29f3205ae9de9640a8c492a2a30f771a4a50a36a
Misheck Livingi <livingim2018@gmail.com>
2023-09-22 17:50:54 +0000

    Sep 22, 2023, 8:50 PM

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -1,8 +1,10 @@
 class User::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
+  before_action :ensure_admin, only: [:new, :create]
+
+  # Assuming you have a 'name' and 'role' attribute on your User model
   validates :name, presence: true
   validates :role, presence: true
-
 
   def new
     super
@@ -20,5 +22,13 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up_path_for(resource)
     root_path
+  end
+
+  private
+
+  def ensure_admin
+    unless current_user && current_user.admin?
+      redirect_to root_path, alert: 'You are not authorized to perform this action.'
+    end
   end
 end
