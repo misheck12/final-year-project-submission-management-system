45c24916639a56558c189073c1583b892ac7349b
Misheck Livingi <livingim2018@gmail.com>
2023-09-22 19:20:49 +0000

    Sep 22, 2023, 10:20 PM

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -1,14 +1,9 @@
 class User::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  before_action :ensure_admin, only: [:new, :create]
+  before_action :ensure_admin_for_faculty_and_staff, only: [:new, :create]
 
-  def new
-    super
-  end
-
-  def create
-    super
-  end
+  # The 'new' and 'create' actions use the default behavior from Devise, 
+  # so you don't need to define them explicitly unless you want to add custom logic.
 
   protected
 
@@ -22,9 +17,13 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def ensure_admin
+  def ensure_admin_for_faculty_and_staff
+    # If the role parameter is missing or it's a student, we allow the registration.
+    return if params[:user].nil? || params[:user][:role] == 'student'
+
+    # For roles other than 'student', we ensure the user is an admin.
     unless current_user && current_user.admin?
-      redirect_to root_path, alert: 'You are not authorized to perform this action.'
+      redirect_to root_path, alert: 'Only admins can register faculty or staff.'
     end
   end
 end
