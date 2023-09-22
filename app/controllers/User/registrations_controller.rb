e15488aa57c4b0c0a7db163b412819fa4dcdf9e6
Misheck Livingi <livingim2018@gmail.com>
2023-09-22 19:43:29 +0000

    Sep 22, 2023, 10:43 PM

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -2,12 +2,23 @@ class User::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :ensure_admin_for_faculty_and_staff, only: [:new, :create]
 
-  # The 'new' and 'create' actions use the default behavior from Devise, 
-  # so you don't need to define them explicitly unless you want to add custom logic.
+  def new
+    super
+  end
+
+  def create
+    super
+  end
 
   protected
 
+  def require_no_authentication
+    return if current_user&.admin?
+    super
+  end
+
   def configure_sign_up_params
+    params[:user][:role] = 'student' unless current_user&.admin?
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
   end
 
@@ -18,12 +29,8 @@ class User::RegistrationsController < Devise::RegistrationsController
   private
 
   def ensure_admin_for_faculty_and_staff
-    # If the role parameter is missing or it's a student, we allow the registration.
-    return if params[:user].nil? || params[:user][:role] == 'student'
-
-    # For roles other than 'student', we ensure the user is an admin.
-    unless current_user && current_user.admin?
-      redirect_to root_path, alert: 'Only admins can register faculty or staff.'
+    if params[:user] && (params[:user][:role] == 'faculty' || params[:user][:role] == 'staff') && !current_user&.admin?
+      redirect_to root_path, alert: 'You are not authorized to perform this action.'
     end
   end
 end
