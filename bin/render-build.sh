0738651167f8a98b95b6b23c2776df2428914506
Misheck Livingi <livingim2018@gmail.com>
2023-09-23 13:11:55 +0200

    update file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,17 @@
 # exit on error
 set -o errexit
 
+# Install gems
 bundle install
+
+# Precompile assets
 bundle exec rake assets:precompile
+
+# Clean up old assets
 bundle exec rake assets:clean
+
+# Run database migrations
 bundle exec rake db:migrate
+
+# Seed the database
+bundle exec rake db:seed
