8d28f9eddd8e29f2e7f572d8a617221f395f0468
Misheck Livingi <livingim2018@gmail.com>
2023-09-23 11:10:32 +0000

    Sep 23, 2023, 2:10 PM

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
+# db/seeds.rb
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Check if the admin user already exists, if not, create one
+unless User.exists?(email: 'admin@fyms.com')
+    User.create!(
+      email: 'admin@fyms.com',
+      password: '6474325m',
+      password_confirmation: '6474325m',
+      role: :admin,
+      name: 'Admin User' # Assuming your User model has a name attribute
+    )
+    puts "Admin user created with email: admin@fyms.com and password: 6474325m"
+  else
+    puts "Admin user already exists."
+  end
+  
